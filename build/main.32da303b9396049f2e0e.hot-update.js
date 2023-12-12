e7f6202f2e6ee543edbdf89828b64c1a84a21004
zyad87 <zyadt871@gmail.com>
2023-12-12 14:03:08 +0300

    first commit

---
+++ build/main.32da303b9396049f2e0e.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatecompany_website"]("main",{
+
+/***/ "./src/sass/style.scss":
+/*!*****************************!*\
+  !*** ./src/sass/style.scss ***!
+  \*****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nunmatched \\\"}\\\".\\n   ╷\\n25 │ }\\r\\n   │ ^\\n   ╵\\n  src\\\\sass\\\\blog.scss 25:1  @import\\n  src\\\\sass\\\\style.scss 3:9  root stylesheet\\n    at tryRunOrWebpackError (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5067:12)\\n    at __webpack_require__ (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5024:18)\\n    at C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5095:20\\n    at symbolIterator (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5002:43\\n    at symbolIterator (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n    at timesSync (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:2297:7)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nunmatched \\\"}\\\".\\n   ╷\\n25 │ }\\r\\n   │ ^\\n   ╵\\n  src\\\\sass\\\\blog.scss 25:1  @import\\n  src\\\\sass\\\\style.scss 3:9  root stylesheet\\n    at Object.<anonymous> (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\src\\\\sass\\\\style.scss:1:7)\\n    at C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:452:10\\n    at Hook.eval [as call] (eval at create (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5069:39\\n    at tryRunOrWebpackError (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5067:12)\\n    at __webpack_require__ (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5024:18)\\n    at C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5095:20\\n    at symbolIterator (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n\\nGenerated code for C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\PC\\\\Desktop\\\\company-website\\\\src\\\\sass\\\\style.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nunmatched \\\\\\\"}\\\\\\\".\\\\n   ╷\\\\n25 │ }\\\\r\\\\n   │ ^\\\\n   ╵\\\\n  src\\\\\\\\sass\\\\\\\\blog.scss 25:1  @import\\\\n  src\\\\\\\\sass\\\\\\\\style.scss 3:9  root stylesheet\\\");\");\n\n//# sourceURL=webpack://company-website/./src/sass/style.scss?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c5b63b63ef53c268b0d7")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ build/main.49311b41c6d14492fd75.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatecompany_website"]("main",{
+
+/***/ "./src/sass/style.scss":
+/*!*****************************!*\
+  !*** ./src/sass/style.scss ***!
+  \*****************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n    if(true) {\n      // 1702372003991\n      var cssReload = __webpack_require__(/*! ../../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ \"./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js\")(module.id, {\"publicPath\":\"../\",\"locals\":false});\n      module.hot.dispose(cssReload);\n      module.hot.accept(undefined, cssReload);\n    }\n  \n\n//# sourceURL=webpack://company-website/./src/sass/style.scss?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("390671c1d4aefc6a970b")
+/******/ })();
+/******/ 
+/******/ }
+);
